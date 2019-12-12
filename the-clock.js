f38f9b6377a1f572ec7b1592fe3506f80928ec21
abhigkar <abhinav.golwalkar@gmail.com>
2019-12-12 21:47:42 +0000

    Add files via upload

---
+++ the-clock.js
@@ -0,0 +1,11 @@
+var current="";
+var images="c00,c01,c02,c03".split(",");
+var lastPress = 0;
+
+for(let i=0;i<4;i++){
+  for(let j=0;j<4;j++){
+	let img = require("Storage").read("c"+i+j);
+    g.drawImage(img2,i*60,j*60);
+  }
+}
+
